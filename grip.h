90d7cb30999d23a88e5efee29a56334df465e082
weratyr <evil@killer.magdlos.com>
2009-11-01 16:43:24 +0100

    add Erkennung durch das Pattern

---
+++ grip.h
@@ -0,0 +1,69 @@
+/*
+ * GripBall Project.
+ * 
+ * Copyright 2006 POB-Technology. All rights reserved.
+ * POB-Technology PROPRIETARY/CONFIDENTIAL.
+ * 
+ */
+#ifndef GRIP_H
+#define GRIP_H
+
+
+/**
+ * Servomotor parameter :
+ *
+ * change servomotor number if you connect your grip on the pob-bot
+ *
+ */
+
+#define HEAD_SERVO            0    //servomotor number for head
+#define GRIP_SERVO_OPEN_CLOSE 1    //servomotor number for open/close grip
+#define GRIP_SERVO_UP_DOWN    2    //servomotor number for up/down grip
+
+
+/**
+ * Grip parameter : 
+ *
+ * you can change this value for your grip in the RgbAndServo program
+ * see www.pob-technology.com/pobdev for download this program.
+ *
+ * GRIP_CLOSE_POSITION : the two component of the grip must be close
+ *
+ * GRIP_MIDDLE_POSITION : grip must be horizontal
+ *
+ * HEAD_INIT_POSITION : Head must be vertical
+ *
+ */
+
+#define GRIP_CLOSE_POSITION   162   //servomotor grip close position
+
+#define GRIP_MIDDLE_POSITION  50 //servomotor grip middle position
+
+#define HEAD_INIT_POSITION    183 //vomotor head position
+
+
+
+// warning, if you change value after this line, grip and head detection can failed !
+
+/**
+ * Grip open/close and catch ball
+ */
+#define GripOpen()	SetServoMotor(GRIP_SERVO_OPEN_CLOSE, GRIP_CLOSE_POSITION-60)
+#define GripClose()	SetServoMotor(GRIP_SERVO_OPEN_CLOSE,GRIP_CLOSE_POSITION)
+#define GripCatchBall()	 SetServoMotor(GRIP_SERVO_OPEN_CLOSE,GRIP_CLOSE_POSITION-10)
+
+/** 
+ * Grip up/down and middle position
+ */
+#define GripDown()	 SetServoMotor(GRIP_SERVO_UP_DOWN,GRIP_MIDDLE_POSITION-45)
+#define GripMiddle() SetServoMotor(GRIP_SERVO_UP_DOWN,GRIP_MIDDLE_POSITION-20)
+#define GripUp()     SetServoMotor(GRIP_SERVO_UP_DOWN,GRIP_MIDDLE_POSITION-10)
+
+/**
+ * Head initial position, up and down
+ */
+#define HeadInit()       SetServoMotor(HEAD_SERVO,HEAD_INIT_POSITION)
+#define HeadUp()         SetServoMotor(HEAD_SERVO,HEAD_INIT_POSITION)
+#define HeadDownToBall() SetServoMotor(HEAD_SERVO,HEAD_INIT_POSITION-68)
+
+#endif
--- main.c
+++ main.c
@@ -1,10 +1,23 @@
-/**
- *  Main program
- */
 #include <pob-eye.h>
+#include <pob-proto.h>
 
+
+#include "grip_pattern.h" //Pattern file
+#include "grip_bitmap.h"  //Bitmap file
+
+#include "grip.h" //Servomotor grip order
+
+
+//function for pob-proto and move bot
+static void InitPobProto (void);
+static void MoveAndStop(UInt8 Way,UInt32 time);
+static void MoveBot(UInt8 Way);
 
 //move order for pob-proto board
+#define DISTANCE_SENSOR_FRONT 5
+#define DISTANCE_SENSOR_RIGHT 2 
+#define DISTANCE_SENSOR_LEFT  3
+
 #define MOVE_RUN		0x05
 #define MOVE_BACK		0x0A
 #define MOVE_LEFT		0x09
@@ -12,43 +25,252 @@
 #define STOP_BOT		0
 
 
+//array for display real time video
+static UInt8         LCD_Fast_Buffer[64*64*BYTES];
+static GraphicBuffer LCD_Fast_Buffer_Video;
+
+static GraphicBuffer LCD_Fast_Buffer_Video1;
+
+//array for display the graphic interface
+static GraphicBuffer LCD_Buffer_Video;
+static UInt8         LCD_Buffer[64*64*BITS];
+
+//function for displat real time video
+static void DrawVision(RGBFrame *ptr);
+static void DrawVisionRight(RGBFrame *ptr);
+
+
+int sensorFront ;
+int sensorLeft ;
+int sensorRight ;
+
 //main program
 int main(void)
 {
+    
 	int i ;
 	RGBFrame rgbFrame ;
 	HSLFrame hslFrame ;
-
-	int exitHouseLoop = 0 ;
+	
+	int exitLoop = 0 ;
 	UInt8 NbForm ;
 	Form ListOfForm[MAX_OF_FORM];
-
-	int BallX = 0 ;
-	int BallY = 0 ;
-
+	
+	
 	InitPOBEYE();
-
 	InitLCD();
-
+	
 	InitPobProto();
 	SwitchOnAllServo();
-
+	
 	//init graphic buffer
 	InitGraphicBuffer(&LCD_Buffer_Video,64,64,ONE_BIT,LCD_Buffer); 
-
 	InitGraphicBuffer(&LCD_Fast_Buffer_Video,64,64,EIGHT_BITS,LCD_Fast_Buffer);
 	ClearGraphicBuffer(&LCD_Fast_Buffer_Video);
-
+	
 	//init frame pointer
 	GetPointerOnRGBFrame(&rgbFrame);
-
 	GetPointerOnHSLFrame(&hslFrame);
-
+	
 	//init head
 	HeadInit();
 	HeadUp();
+	
+	//init grip
+	GripMiddle();
+	GripOpen();
+	GripDown();
+	
+	//set head to see ball
+	HeadDownToBall();
+	ClearGraphicBuffer(&LCD_Buffer_Video);
+	ClearGraphicBuffer(&LCD_Buffer_Video);
+	//house loop
+	
+    while( exitLoop == 0 )
+	{
+		GrabRGBFrame();
+		BinaryRGBFrame(&rgbFrame);
+		DrawVision(&rgbFrame);
+		
+		sensorFront = GetPortAnalog(DISTANCE_SENSOR_FRONT);
+		sensorLeft = GetPortAnalog(DISTANCE_SENSOR_LEFT);
+		sensorRight = GetPortAnalog(DISTANCE_SENSOR_RIGHT);
+		
+		//draw camera frame
+		DrawVision(&rgbFrame);
+		
+		NbForm = IdentifyForm(&rgbFrame,ListOfForm,grip_pattern);								
+		
+		if( NbForm == 0 )
+		{
+			//such den becher
+			
+			MoveAndStop(MOVE_LEFT,30000);
+			MoveAndStop(MOVE_RUN,90000);
+		}
+		else 
+		{ 
+			exitLoop = GoToCup(ListOfForm, NbForm); 
+		}
+        
+        PrintTextOnPobTerminal("Front Sensor %d, %d, %d", sensorFront, sensorLeft, sensorRight);
+	    PrintTextOnPobTerminal("IdentifyForm value %d ", NbForm );
+		
+	}
+	
+	return 0;
+}
 
+
+/** Go to the cup function.
+ *
+ * @param formArray : pointer to a list of form in the frame
+ * @param nbForm : form's number
+ *
+ * @return -1 if bot is just front of the cup, 0 else.
+ *
+ */
+int GoToCup( Form *formArray, int nbForm)
+{
+	int i ;
+	int MoveOrder = STOP_BOT ;
+	int HeadPosition = 110 ;
+	
+	for(i=0 ; i<nbForm ; i++ )
+	{
+		if( formArray[i].id == IDP_BECHER )
+		{
+//			//move head to center the form
+//			if( formArray[i].y>65)
+//			{
+//				HeadPosition = HeadPosition - 4;
+//			}
+//			else if( formArray[i].y<55)
+//			{
+//				HeadPosition = HeadPosition + 4;
+//			}
+			
+		//	SetServoMotor(HEAD_SERVO,HeadPosition);
+			
+			
+            PrintTextOnPobTerminal("form at w=%d h=%d", formArray[i].x ,formArray[i].y );
+			//move bot to center the form
+			if( formArray[i].x > 55 )
+			{
+				MoveAndStop(MOVE_RIGHT,40000);
+			}
+			else if( formArray[i].x < 35 )
+			{
+				MoveAndStop(MOVE_LEFT,40000);
+			}
+			else //go to cup with the bot
+			{
+				//becher sollte in der mitte des Bildes sein
+				
+				//fragt den Frontsensor ab
+				sensorFront = GetPortAnalog(DISTANCE_SENSOR_FRONT); 
+				
+				if( formArray[i].width > 20 && formArray[i].height > 40 &&
+				   formArray[i].width < 25 && formArray[i].height < 45)
+				{
+					//go slow 
+					MoveBot(STOP_BOT);
+					MoveAndStop(MOVE_RUN,20000);
+				}
+				else if( formArray[i].width > 30 && formArray[i].height > 50 )
+				{
+					MoveAndStop(MOVE_RUN,90000);
+					if (sensorFront > 90) {
+						//stop move and open gri
+						GripClose();
+						Wait(200000);
+						return -1 ;
+                    }
+				}
+				else
+				{
+					//go fast to the house
+					MoveOrder=MOVE_RUN;
+				}
+				
+			}
+			
+		}
+	}
+	
+	MoveBot(MoveOrder);
+	
+	return 0 ;
+}
+
+
+
+/**
+ * Initialize the POB-PROTO board. PORTA set as analogic, PORTC as servomotor.
+ */
+void InitPobProto (void)
+{
+	PobProto board;
 	
 	
+	//to get the position of the analogic joystick, you have to set the PORTA as analogic input
+	board.porta=ALL_PORTA_AS_ANA;	
+	
+	//all pin of PORTC are configured to manage servomotors
+	board.portc=RC7_AS_SERVO |RC6_AS_SERVO |RC3_AS_SERVO |RC2_AS_SERVO |RC1_AS_SERVO |RC0_AS_SERVO;
+	
+	
+	//RD0 RD1 RD2 RD3 are configured as digitals output to gear DC motor
+	//RD4 RD5 RD6 RD7 are configured as digitals input
+	board.portd=RD7_AS_DI |RD6_AS_DI |RD5_AS_DI |RD4_AS_DI |RD3_AS_DO |RD2_AS_DO |RD1_AS_DO |RD0_AS_DO;
+	
+	//set the pob proto
+	SetPobProto(&board);
 	
 }
+
+/**
+ * Move and stop bot.
+ *
+ * @param Way : move order
+ * @param time : time to move
+ */
+void MoveAndStop( UInt8 Way, UInt32 time )
+{
+	MoveBot(Way);
+	Wait(time);
+	MoveBot(STOP_BOT);
+	Wait(400000);
+}
+
+/**
+ * Move the bot
+ *
+ * @param Way : move bot order
+ */
+void MoveBot (UInt8 Way)
+{	
+	SetPortD(Way);
+}
+
+/** 
+ * Draw the frame binarised 
+ *
+ * @param ptr : pointer to a valid RGBFrame
+ */
+void DrawVision (RGBFrame *ptr)
+{	
+	//draw the red buffer (88*120 pixels) on the Buffer for for the left screen (64*64 pixels)
+	DrawComponentInABufferVideo(ptr->red,&LCD_Fast_Buffer_Video);
+	
+	DrawLeftLCD(&LCD_Fast_Buffer_Video); //refresh the left screen
+}
+
+
+void DrawVisionRight(RGBFrame *ptr) 
+{
+	DrawComponentInABufferVideo(ptr->red,&LCD_Fast_Buffer_Video);
+	DrawRightLCD(&LCD_Fast_Buffer_Video);
+}
+
